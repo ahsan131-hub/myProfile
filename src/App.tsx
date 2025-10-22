d9e74a5d2622e5e71fc3457a87f07171d8a2d952
ahsan <ahsanjsdev@gmail.com>
2025-10-22 13:00:23 +0200

    Update routing in App component to set Museum as the default page, replacing the previous museum route.

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             {/* <Route path="/" element={<Index />} /> */}
-            <Route path="/museum" element={<Museum />} />
+            <Route path="/" element={<Museum />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
