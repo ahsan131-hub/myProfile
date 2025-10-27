772f45da686820f230ec5e3172554eccc45edb20
ahsan <ahsanjsdev@gmail.com>
2025-10-27 17:10:38 +0100

    Remove DigiAccess script from index.html, add Swiper dependency in package-lock.json, and clean up unused import in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
-import Index from "./pages/Index";
 import Museum from "./pages/Museum";
 import NotFound from "./pages/NotFound";
 
